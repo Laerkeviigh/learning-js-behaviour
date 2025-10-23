5050a91147f44e252f682192dca3b029111b62f5
Lærke G. Vigh <eaa25lgv@students.eaaa.dk>
2025-10-23 15:57:02 +0200

    changes

--- 05/js/script.js
+++ 05/js/script.js
@@ -1,19 +1,14 @@
 "use strict";
 
-
 const changeImgBtn = document.getElementById("changeImage");
 const getImage = document.getElementById("imagePlaceholder");
 
-
-
 changeImgBtn.addEventListener("click", function() {
-
-    if (this.textContent == "Before"){
-        getImage.src= "img/before.png";
-        this.textContent = "After";
-    }
-    else {
-        getImage.src= "img/after.png";
-        this.textContent = "Before";
-     }
+  if (this.textContent == "Winter") {
+    getImage.src = "img/winter.jpg";
+    this.textContent = "Autumn";
+  } else {
+    getImage.src = "img/autumn.jpg";
+    this.textContent = "Winter";
+  }
 });
