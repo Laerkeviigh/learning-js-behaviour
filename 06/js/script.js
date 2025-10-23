70d83c400af9b2d70af142f652c407b5298b3c1a
Lærke G. Vigh <eaa25lgv@students.eaaa.dk>
2025-10-23 16:15:59 +0200

    changes

--- 06/js/script.js
+++ 06/js/script.js
@@ -1,19 +1,19 @@
 "use strict";
 
-
 const getZoomBtn = document.getElementById("zoomBtn");
 const getWordElem = document.getElementById("word");
 
-
-
 getZoomBtn.addEventListener("click", function() {
 
-    if (this.textContent == "Zoom ind"){
-        getWordElem.style.fontSize = "6rem";
+    if (this.textContent == "Zoom ind") {
+        getWordElem.style.fontSize = "5rem";               // ny størrelse
+        getWordElem.style.fontFamily = "'Courier New', monospace"; // ny skrifttype
+        getWordElem.style.color = "pink";              // ny farve
         this.textContent = "Zoom ud";
-    }
-    else {
-        getWordElem.style.fontSize = "3.0rem";
+    } else {
+        getWordElem.style.fontSize = "3rem";              // tilbage til standard
+        getWordElem.style.fontFamily = "'Segoe UI', Tahoma, Geneva, Verdana, sans-serif"; // tilbage til standard
+        getWordElem.style.color = "darkcyan";            // tilbage til standard
         this.textContent = "Zoom ind";
-     }
+    }
 });
--- 07/js/script.js
+++ 07/js/script.js
@@ -21,14 +21,33 @@
 // });
 
 
+"use strict";
+
 const getWordElem = document.getElementById("word");
-
 getWordElem.addEventListener("click", function() {
-
-    if (this.textContent == "Det ta'r kun 5 minutter"){
+    if (this.textContent == "Det ta'r kun 5 minutter") {
         this.innerHTML = "<strong>og så er du i Netto.</strong>";
-    }
-    else {
+    } else {
         this.innerHTML = "Det ta'r kun 5 minutter";
-     }
+    }
+});
+
+// nike slogan
+const getSlogan2 = document.getElementById("slogan2");
+getSlogan2.addEventListener("click", function() {
+    if (this.textContent == "Just Do It") {
+        this.innerHTML = "<strong>Nike</strong>";
+    } else {
+        this.innerHTML = "Just Do It";
+    }
+});
+
+// jysk slogan
+const getSlogan3 = document.getElementById("slogan3");
+getSlogan3.addEventListener("click", function() {
+    if (this.textContent == "Et godt tilbud for alle, dag og nat") {
+        this.innerHTML = "<strong>Jysk</strong>";
+    } else {
+        this.innerHTML = "Et godt tilbud for alle, dag og nat";
+    }
 });
